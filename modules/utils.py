542c6956f36febb53ebbbc22643aa6fa77dc9f10
BnbBillionaire <99882254+BILLIMANCRYPTO@users.noreply.github.com>
2024-03-28 11:03:15 +0300

    Add files via upload

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,7 @@
 rpc_endpoints = {
     "1": "https://rpc.ankr.com/eth",
-    "2": "https://rpc.ankr.com/gnosis",
+    "2": "https://gnosis.drpc.org",
     "3": "https://rpc.ankr.com/eth",
-    "4": "https://rpc.ankr.com/arbitrum"
+    "4": "https://rpc.ankr.com/arbitrum",
+    "5": "https://optimism.drpc.org"
 }
